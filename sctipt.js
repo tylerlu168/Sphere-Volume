026e2252113455a87f55de352e031438b19514bb
tylerlu168 <tylerlu168@gmail.com>
2024-09-08 19:54:45 -0400

    Add files via upload

---
+++ sctipt.js
@@ -0,0 +1,8 @@
+let rad = document.getElementById("radius").value;
+
+function calculate()
+{
+    let r = inputElement.value;
+    document.getElementById("output").innerText = rad;
+    document.getElementById("output").innerText = "Volume: " + volume;
+}
